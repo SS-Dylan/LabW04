a47b65c1f36a98babedd9325f1c2398ce3ef7d53
Dylan Shaffer <SHAFFERDR@ETSU.EDU>
2022-09-14 10:39:00 -0400

    Add project files.

---
+++ DSWebAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSWebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
